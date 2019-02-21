49dca7c0389ead49d02fc66022b8ab0a848378de
thisisfruit <42990686+thisisfruit@users.noreply.github.com>
2019-02-21 12:22:39 +0800

    Homework Uploader

---
+++ index.js
@@ -0,0 +1,75 @@
+const
+    restify = require('restify'),
+    config = require('config'),
+    builder = require('botbuilder'),
+    apiAIRecognizer = require('api-ai-recognizer'),
+    request = require('request');
+
+const
+    DIALOGFLOW_CLIENT_ACCESS_TOKEN = config.get('dialogFlowClientAccessToken'),
+    SHEETDB_API_ID = config.get('SheetDB_API_id'),
+    MICROSOFT_APP_ID = config.get('Microsoft_APP_id'),
+    MICROSOFT_APP_PASSWORD = config.get('Microsoft_APP_password');
+
+var recognizer = new apiAIRecognizer(DIALOGFLOW_CLIENT_ACCESS_TOKEN);
+var intents = new builder.IntentDialog({recognizers:[recognizer]})
+var server = restify.createServer();
+server.listen(process.env.PORT || process.env.port || 3978,
+    function(){
+        console.log('%s listening to %s', server.name, server.url)
+    });
+
+var connector = new builder.ChatConnector({
+    appId: MICROSOFT_APP_ID,
+    appPassword:MICROSOFT_APP_PASSWORD
+})
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+var bot = new builder.UniversalBot(connector).set('storage',inMemoryStorage);
+server.post('/api/messages',connector.listen());
+bot.dialog('/',intents);
+
+intents.matches('Summit Final Project', function(session,args){
+    var checkAction = builder.EntityRecognizer.findEntity(args.entities,"actionIncomplete");
+    var studentIDObject = {},
+        studentNameObject = {},
+        studentWebsiteObject = {};
+    if(checkAction.entity)
+    {
+        var myFulfillment = builder.EntityRecognizer.findEntity(args.entities,'fulfillment');
+        session.send(myFulfillment.entity);
+    }else
+    {
+        studentIDObject = builder.EntityRecognizer.findEntity(args.entities,'StudentID');
+        studentNameObject = builder.EntityRecognizer.findEntity(args.entities,'StudentName');
+        studentWebsiteObject = builder.EntityRecognizer.findEntity(args.entities,'StudentWebsite');
+        session.send("好喔！ %s , 你的學號是 %s, 你的期末作業是 %s 馬上為你登記！", studentNameObject.entity, studentIDObject.entity, session.message.text
+        );
+        summitToSheetDB(studentIDObject.entity,studentNameObject.entity,session.message.text,session);
+    }
+});
+
+intents.matchesAny(['Default Fallback Intent','Default Welcome Intent','None'],function(session,args)
+{
+    session.send('想交作業嗎？請說[我想要上傳作業]、[我想要交作業]一類的詞，謝謝你！');
+});
+function summitToSheetDB(sID, sName, sUrl, session){
+    console.log(sID, sName, sUrl);
+    request({
+        uri:'https://sheetdb.io/api/v1/'+SHEETDB_API_ID,
+        json:true,
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:{
+            'data':[{
+                "Number":sID, "Name":sName, 'Website':sUrl
+            }]
+        }
+    },function(error, response, body){
+        session.send('sheetDB完成!');
+
+        if(!error && response.statusCode ==201){
+            session.send('作業上傳完成!');
+        }else{session.send(error)}   
+})
+}
